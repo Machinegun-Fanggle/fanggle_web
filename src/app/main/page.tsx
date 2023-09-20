f92e26d7bbeb39f690c0b9674c665cb752c73f83
김규현 <rbxlr5848@gamil.com>
2023-09-20 18:26:50 +0900

    ...

---
+++ src/app/main/page.tsx
@@ -0,0 +1,19 @@
+//app/signin/page.tsx
+
+"use client";
+
+
+export default function Login() {
+
+
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <h1>Login</h1>
+            <h1>Login</h1>
+            <h1>Login</h1>
+
+        </div>
+    );
+}
