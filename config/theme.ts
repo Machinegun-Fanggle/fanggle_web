c078d3988c7e06e0608a4cc7b0dccea8cb900c02
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-22 08:14:39 +0900

    feat: 피그마 기반 테마 반영

---
+++ config/theme.ts
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    primary: '#0094FF',
+    secondary: '#A9B2B9',
+    text: {
+      default: '#353535',
+      light: '#767676',
+    },
+  },
+}
+
+export default theme
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
+import theme from 'config/theme'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    '.storybook/*.{ts,js}',
   ],
   theme: {
-    extend: {},
+    colors: theme.colors,
+    extend: theme,
   },
   plugins: [],
 }
-
