943426627ae061f4d54cdcad844feeeed1caa8b1
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-22 11:57:56 +0900

    feat: 스토리북 환경설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,34 @@
+import path from 'path';
 import type { StorybookConfig } from "@storybook/nextjs"
 
 const config: StorybookConfig = {
-    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-    addons: [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@storybook/addon-onboarding",
-        "@storybook/addon-interactions",
-    ],
-    framework: {
-        name: "@storybook/nextjs",
-        options: {},
-    },
-    docs: {
-        autodocs: "tag",
-    },
-}
-export default config
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  webpackFinal: async (config: any) => {
+    console.log("path " + path.resolve(__dirname, '../src/app/components'));
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@src': path.resolve(__dirname, '../src'),
+      '@component': path.resolve(__dirname, '../src/app/components'),
+      '@public': path.resolve(__dirname, '../src/public'),
+      '@svg': path.resolve(__dirname, '../src/app/components/svg'),
+      '@lib': path.resolve(__dirname, '../src/app/lib'),
+      '@util': path.resolve(__dirname, '../src/app/util'),
+    };
+    return config;
+  },
+};
+
+export default config;
