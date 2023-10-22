55940c8ebcdd6a12bd89412d16fbd81b3426b9f3
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-23 04:19:58 +0900

    fix: eslint fix

--- src/app/api/apiInstance.ts
+++ src/app/api/apiInstance.ts
@@ -1,5 +1,5 @@
-import axios from "axios"
+import axios from 'axios';
 
 export const apiInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-})
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+});
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,108 +1,107 @@
 //app/api/auth/[...nextauth]/route.ts
 
-import NextAuth from "next-auth/next"
-import CredentialsProvider from "next-auth/providers/credentials"
-import GoogleProvider from "next-auth/providers/google"
-import KakaoProvider from "next-auth/providers/kakao"
-import NaverProvider from "next-auth/providers/naver"
+import NextAuth from 'next-auth/next';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import GoogleProvider from 'next-auth/providers/google';
+import KakaoProvider from 'next-auth/providers/kakao';
+import NaverProvider from 'next-auth/providers/naver';
 
-import { apiInstance } from "@/app/api/apiInstance"
+import { apiInstance } from '@src/app/api/apiInstance';
 
 const handler = NextAuth({
-    providers: [
-        CredentialsProvider({
-            // The name to display on the sign in form (e.g. "Sign in with...")
-            name: "Credentials",
-            // `credentials` is used to generate a form on the sign in page.
-            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-            // e.g. domain, username, password, 2FA token, etc.
-            // You can pass any HTML attribute to the <input> tag through the object.
-            credentials: {
-                username: {
-                    label: "이메일",
-                    type: "text",
-                    placeholder: "이메일 주소 입력 요망",
-                },
-                password: { label: "비밀번호", type: "password" },
-            },
-
-            async authorize(credentials, req) {
-                const res = await fetch(
-                    `${process.env.NEXTAUTH_URL}/api/login`,
-                    {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                        body: JSON.stringify({
-                            username: credentials?.username,
-                            password: credentials?.password,
-                        }),
-                    }
-                )
-                const user = await res.json()
-
-                //console.log(res)
-
-                if (user) {
-                    // Any object returned will be saved in `user` property of the JWT
-                    return user
-                } else {
-                    // If you return null then an error will be displayed advising the user to check their details.
-                    return null
-
-                    // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-                }
-            },
-        }),
-        GoogleProvider({
-            clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "",
-            clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET || "",
-        }),
-
-        KakaoProvider({
-            clientId: process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID!,
-            clientSecret: process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET!,
-        }),
-
-        NaverProvider({
-            clientId: process.env.NEXT_PUBLIC_NAVER_CLIENT_ID!,
-            clientSecret: process.env.NEXT_PUBLIC_NAVER_CLIENT_SECRET!,
-        }),
-    ],
-
-    callbacks: {
-        async jwt({ token, user, account, isNewUser }) {
-            // console.log(user)
-
-            if (account) {
-                await SetSignIn([token, user, account, isNewUser])
-
-                console.log(account?.provider)
-                console.log(token)
-                console.log(user)
-            }
-
-            return { ...token, ...user }
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: 'Credentials',
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: {
+          label: '이메일',
+          type: 'text',
+          placeholder: '이메일 주소 입력 요망',
         },
+        password: { label: '비밀번호', type: 'password' },
+      },
 
-        async session({ session, token }) {
-            session.user = token as any
-            await SetSignIn([token, session])
-            return session
-        },
+      async authorize(credentials, req) {
+        const res = await fetch(`${process.env.NEXTAUTH_URL}/api/login`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            username: credentials?.username,
+            password: credentials?.password,
+          }),
+        });
+        const user = await res.json();
+
+        //console.log(res)
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET || '',
+    }),
+
+    KakaoProvider({
+      clientId: process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID!,
+      clientSecret: process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET!,
+    }),
+
+    NaverProvider({
+      clientId: process.env.NEXT_PUBLIC_NAVER_CLIENT_ID!,
+      clientSecret: process.env.NEXT_PUBLIC_NAVER_CLIENT_SECRET!,
+    }),
+  ],
+
+  callbacks: {
+    async jwt({ token, user, account, isNewUser }) {
+      // console.log(user)
+
+      if (account) {
+        await SetSignIn([token, user, account, isNewUser]);
+
+        console.log(account?.provider);
+        console.log(token);
+        console.log(user);
+      }
+
+      return { ...token, ...user };
     },
 
-    // pages: {
-    //     signIn: "/signin",
-    // },
-})
+    async session({ session, token }) {
+      session.user = token as any;
+      await SetSignIn([token, session]);
+      return session;
+    },
+  },
+
+  // pages: {
+  //     signIn: "/signin",
+  // },
+});
 
 const SetSignIn = async (data) => {
-    try {
-        const response = await apiInstance.post(`/account/signup`, data)
-        console.log(response)
-    } catch (error) {}
-}
+  try {
+    const response = await apiInstance.post(`/account/signup`, data);
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
