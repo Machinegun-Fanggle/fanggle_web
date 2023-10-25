8c920bd4b4a595c830c77bd054c90d84b3e55ecd
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-25 12:03:51 +0900

    fix: 잘못된 테마 값 변경

--- config/theme.ts
+++ config/theme.ts
@@ -3,10 +3,8 @@ const theme = {
     white: '#FFFFFF',
     primary: '#5081FF',
     secondary: '#A9B2B9',
-    text: {
-      default: '#353535',
-      light: '#767676',
-    },
+    default: '#353535',
+    light: '#767676',
   },
   boxShadow: {
     md: '0px 4px 4px rgba(217, 214, 214, 0.25)',
