5d974b03457361fbb4f537be3b275a4a78a51583
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-27 10:24:05 +0900

    feat: 테마 color 추가

--- config/theme.ts
+++ config/theme.ts
@@ -5,6 +5,7 @@ const theme = {
     secondary: '#A9B2B9',
     default: '#353535',
     light: '#767676',
+    purple: '#6070FF',
   },
   boxShadow: {
     md: '0px 4px 4px rgba(217, 214, 214, 0.25)',
