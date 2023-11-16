0408b944aedf672629f3d715126726ab19dff419
dev-wina <debugingsy@gmail.com>
2023-11-16 09:23:58 +0900

    ...

--- src/app/easyjoin/page.tsx
+++ src/app/easyjoin/page.tsx
@@ -109,6 +109,7 @@ export default function EformSignPage() {
   };
 
   // 서명키를 사용하여 이폼사인에서 발급받은 Access Token을 요청
+  // eslint-disable-next-line no-unused-vars
   async function getAccessTokenFromEformsign(param: SignitureBody) {
     // 1. Authorize: 이폼사인에서 발급받은 API 키를 Base64로 인코딩한 값 입력
     const authorizeValue = encodeToBase64(apiKey);
@@ -140,14 +141,14 @@ export default function EformSignPage() {
   }
 
   // eslint-disable-next-line no-unused-vars
-  async function refreshAccessToken() {
+  async function refreshAccessToken(param: SignitureBody) {
     // 1. Authorize: 이폼사인에서 발급받은 API 키를 Base64로 인코딩한 값 입력
     const authorizeValue = encodeToBase64(apiKey);
 
     // 2. Header: 생성한 서명값(eformsign_signature) (* 참고: API 키 발급 시 설정한 검증유형에 따라 다름)
     const headers = {
       Accept: 'application/json;charset=utf-8',
-      // eformsign_signature: param.signature,
+      eformsign_signature: param.signature,
       Authorization: 'Bearer ' + authorizeValue, // ! API 키를 Base64로 인코딩한 값 앞에 'Bearer ' 추가헤야 동작함!
       'Content-Type': 'application/json;charset=utf-8',
     };
@@ -255,6 +256,7 @@ export default function EformSignPage() {
     }
   };
 
+  // eslint-disable-next-line no-unused-vars
   const createTemplateWithMyOwnDocs = async () => {
     if (window.EformSignTemplate) {
       const eformsign = new window.EformSignTemplate();
@@ -274,13 +276,15 @@ export default function EformSignPage() {
     // 1. 서명 생성
     // 2. 서명키를 사용하여 이폼사인에서 발급받은 Access Token을 요청
     // 3. 내 문서로 템플릿 생성
-    createSignature().then((data: SignitureBody) =>
-      getAccessTokenFromEformsign(data).then(() => {
-        createTemplateWithMyOwnDocs();
-        refreshAccessToken();
-        getDocumentList();
-      })
-    );
+    // createSignature().then((data: SignitureBody) =>
+    //   getAccessTokenFromEformsign(data).then(() => {
+    //     createTemplateWithMyOwnDocs();
+    //     refreshAccessToken();
+    //     getDocumentList();
+    //   })
+    // );
+
+    createSignature().then((data: SignitureBody) => refreshAccessToken(data));
   }, []);
 
   return (
